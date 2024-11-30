32e0d9cca4431ce31bf5ff991dd198e5e08b4e1c
ZAMMELI <aymen.01.zammeli@gmail.com>
2024-11-30 08:26:52 +0100

    first commit v0.0

---
+++ src/main/java/com/Gestion_Bib/demo/GestionBibApplication.java
@@ -0,0 +1,13 @@
+package com.Gestion_Bib.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionBibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionBibApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Gestion_Bib/demo/GestionBibApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Gestion_Bib.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionBibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
